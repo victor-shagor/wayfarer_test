09b3e3d3acf3e756fc4b0d99e68cd3fd8e3b998d
victor-shagor <ojoabiolavictor@gmail.com>
2019-07-06 06:30:28 +0100

    swagger

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import tripRouter from './server/routes/tripRouter';
 
 
 const app = express();
-const swaggerDoc = YAML.load(`${process.cwd()}/swagger.yaml`);
+const swaggerDoc = YAML.load('./swagger.yaml');
 app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
